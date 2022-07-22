d8900ee59e0fc817ca19bda2b729ef1b11aaba3d
Claire <clairelynndickerson@gmail.com>
2022-07-22 18:58:52 -0500

    Twitch attempt 7 New Errorgit add .git add .!

--- twitch.js
+++ twitch.js
@@ -3,7 +3,7 @@ let options = {
     height: 480,
     channel: "clairelynnd",
     // Only needed if this page is going to be embedded on other websites
-    parent: ["main--euphonious-hotteok-de570c.netlify.app/","euphonious-hotteok-de570c.netlify.app/twitch.html"]
+    parent: ["euphonious-hotteok-de570c.netlify.app"]
       };
 
 let player = new Twitch.Player("twitch-embed", options);
