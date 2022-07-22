867ff629c31aaab764033e3fab6adceb57ba387e
Claire <clairelynndickerson@gmail.com>
2022-07-22 18:54:02 -0500

    Twitch attempt 6

--- twitch.js
+++ twitch.js
@@ -3,7 +3,7 @@ let options = {
     height: 480,
     channel: "clairelynnd",
     // Only needed if this page is going to be embedded on other websites
-    parent: ["https://main--euphonious-hotteok-de570c.netlify.app/","https://euphonious-hotteok-de570c.netlify.app/twitch.html"]
+    parent: ["main--euphonious-hotteok-de570c.netlify.app/","euphonious-hotteok-de570c.netlify.app/twitch.html"]
       };
 
 let player = new Twitch.Player("twitch-embed", options);
