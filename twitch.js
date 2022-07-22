90184ee5d4d1d49b0488c80bc0cc81d783ecea33
Claire <clairelynndickerson@gmail.com>
2022-07-22 17:08:03 -0500

    Change twitch.js child object

--- twitch.js
+++ twitch.js
@@ -3,5 +3,5 @@ new Twitch.Embed("twitch-embed", {
     height: 480,
     channel: "clairelynnd",
     // Only needed if this page is going to be embedded on other websites
-    parent: ["https://www.twitch.tv/clairelynnd", "othersite.example.com"]
+    parent: ["https://www.twitch.tv/clairelynnd", "https://euphonious-hotteok-de570c.netlify.app/twitch.html"]
       });
