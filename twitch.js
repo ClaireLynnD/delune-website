c2114b19ad335af04c180938b152b2d2e874ac79
Claire <clairelynndickerson@gmail.com>
2022-07-22 17:01:53 -0500

    Create twitch.js

---
+++ twitch.js
@@ -0,0 +1,7 @@
+new Twitch.Embed("twitch-embed", {
+    width: 854,
+    height: 480,
+    channel: "clairelynnd",
+    // Only needed if this page is going to be embedded on other websites
+    parent: ["https://www.twitch.tv/clairelynnd", "othersite.example.com"]
+      });
